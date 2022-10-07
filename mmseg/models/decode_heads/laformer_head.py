6ffd63ca561694f509acfab41e220e91373fb51e
main-2983 <nguyen.mai@sun-asterisk.com>
2022-10-07 09:25:04 +0700

    Wrong model, push correct one

--- mmseg/models/decode_heads/laformer_head.py
+++ mmseg/models/decode_heads/laformer_head.py
@@ -9,11 +9,14 @@ from mmseg.models.utils import SELayer
 
 
 @HEADS.register_module()
-class LAFormerHead(BaseDecodeHead):
+class LAPFormerPPHead_v2(BaseDecodeHead):
     def __init__(self,
                  interpolate_mode='bilinear',
                  **kwargs):
-        super(LAFormerHead, self).__init__(input_transform='multiple_select', **kwargs)
+        super(LAPFormerPPHead_v2, self).__init__(
+            input_transform='multiple_select',
+            **kwargs
+        )
         self.interpolate_mode = interpolate_mode
         num_inputs = len(self.in_channels)
 
@@ -40,24 +43,15 @@ class LAFormerHead(BaseDecodeHead):
                     kernel_size=1,
                     stride=1,
                     norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg))
-
-        self.sep_conv = nn.ModuleList([
-            nn.Sequential(
-                nn.Conv2d(self.channels, self.channels, 1),
-                nn.Conv2d(self.channels,
-                          self.channels,
-                          kernel_size=2,
-                          stride=2)
-            ),
-            nn.Conv2d(self.channels, self.channels, 1)
-        ])
+                    act_cfg=self.act_cfg
+                )
+            )
 
         self.se_module = SELayer(
-            channels=self.channels * (num_inputs)
+            channels=self.channels * (num_inputs - 1)
         )
         self.fusion_conv = ConvModule(
-            in_channels=self.channels * (num_inputs),
+            in_channels=self.channels * (num_inputs - 1),
             out_channels=self.channels,
             kernel_size=1,
             norm_cfg=self.norm_cfg)
@@ -80,12 +74,11 @@ class LAFormerHead(BaseDecodeHead):
             _inputs.append(feat)
 
         # progressive feature fusion
-        # _out to cache features
         _out = torch.empty(
             _inputs[1].shape
         )
-        outs = [_inputs[-1]] # 1/8, 1/8, 1/8, 1/8, 1/4
-        for idx in range(len(inputs) - 1, 0, -1):
+        outs = [_inputs[-1]]
+        for idx in range(len(_inputs) - 1, 0, -1):
             linear_prj = self.linear_projections[idx - 1]
             # cat first 2 from _inputs
             if idx == len(_inputs) - 1:
@@ -95,7 +88,6 @@ class LAFormerHead(BaseDecodeHead):
             else:
                 x1 = _out
                 x2 = _inputs[idx - 1]
-            # upsampling the 2nd scale to size of 1st scale to concat
             if idx == 1:
                 x1 = resize(
                     input=x1,
@@ -105,34 +97,22 @@ class LAFormerHead(BaseDecodeHead):
                 )
             x = torch.cat([x1, x2], dim=1)
             _out = linear_prj(x)
-            # decoupling the 1/4 scale in outs
-            if idx == 1:
-                _out_sep1 = self.sep_conv[0](_out)
-                _out_sep2 = self.sep_conv[1](_out)
-                # resize half of 1/4 to 1/8
-                _out_sep1 = resize(
-                    input=_out_sep1,
-                    size=inputs[1].shape[2:],
-                    mode=self.interpolate_mode,
-                    align_corners=self.align_corners
-                )
-                outs.extend([_out_sep1, _out_sep2])
-            else: # if not 1/4 then append to list
-                outs.append(_out)
+            outs.append(_out)
 
-        out = torch.cat(outs[:4], dim=1)
+        out = torch.cat(outs[:3], dim=1)
         out = self.se_module(out)
         out = self.fusion_conv(out)
-        # upsample all 1/8 to 1/4
         out = resize(
             input=out,
-            size=outs[-1].shape[2:],
+            size=_inputs[0].shape[2:],
             mode=self.interpolate_mode,
             align_corners=self.align_corners
         )
+
         # perform identity mapping
         out = outs[-1] + out
 
         out = self.cls_seg(out)
 
         return out
+
